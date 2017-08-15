a4bda99c2a93eb1470907d499c58ab2c73070636
Prasanna Kumar <spkforwork@gmail.com>
2017-08-15 13:59:55 +0530

    added network code with asynctask

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,8 +17,10 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -29,58 +31,90 @@ import java.util.ArrayList;
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2010-01-01&endtime=2017-08-14&minfelt=50&minmagnitude=5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-//        ArrayList<Earthquake> earthquakes = new ArrayList<Earthquake>();
-//        earthquakes.add(new Earthquake("4.5","San Francisco","Aug 4, 2017"));
-//        earthquakes.add(new Earthquake("5.1","Londoon","Aug 1, 2017"));
-//        earthquakes.add(new Earthquake("7.3","Tokyo","Feb 14, 2017"));
-//        earthquakes.add(new Earthquake("3.7","Mexico City","Jan 24, 2017"));
-//        earthquakes.add(new Earthquake("2.5","Moscow","May 10, 2017"));
-//        earthquakes.add(new Earthquake("4.3","Rio de Janeiro","Jun 4, 2017"));
-//        earthquakes.add(new Earthquake("1.8","Paris","Jul 9, 2017"));
+        try{
+            // Find a reference to the {@link ListView} in the layout
+            ListView earthquakeListView = (ListView) findViewById(R.id.list);
+            if (earthquakeListView != null) {
+                earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
-
-
-
-
-        // Create a new {@link ArrayAdapter} of earthquakes
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-//                this, android.R.layout.simple_list_item_1, earthquakes);
-
-        EarthquakeAdapter earthquakeAdapter = new EarthquakeAdapter(this,earthquakes);
-
-        // Find a reference to the {@link ListView} in the layout
-        ListView earthquakeListView = (ListView) findViewById(R.id.list);
-
-        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // When clicked, show a toast with the TextView text
-//                Toast.makeText(getApplicationContext(),
-//                        ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
-
-                Earthquake entry = (Earthquake) parent.getAdapter().getItem(position);
-                String url = entry.getUrl();
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                startActivity(i);
+                        Earthquake entry = (Earthquake) parent.getAdapter().getItem(position);
+                        String url = entry.getUrl();
+                        Intent i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse(url));
+                        startActivity(i);
+                    }
+                });
             }
-        });
+            else{
+                Toast.makeText(EarthquakeActivity.this, "Unable to set onItemClickListener for list items", Toast.LENGTH_LONG).show();
+            }
+        }
+        catch (NullPointerException e){
+            Log.e(LOG_TAG, "Problem in setting onItemClickListener for list items", e);
+        }
 
+
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
+
+
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, ArrayList<Earthquake>> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected ArrayList<Earthquake> doInBackground(String... urls) {
+
+            //return null if there is no URL
+            if(urls.length < 1 || urls[0] == null){
+                return null;
+            }
+
+            // Perform the HTTP request for earthquake data and process the response.
+
+            return QueryUtils.fetchEarthquakeData(USGS_REQUEST_URL);
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Earthquake> earthquakes) {
+            // Update the information displayed to the user.
+            if(earthquakes == null){
+                return;
+            }
+            updateUI(earthquakes);
+        }
+    }
+
+    void updateUI(ArrayList<Earthquake> earthquakes){
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
+        // Find a reference to the {@link ListView} in the layout
+        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        EarthquakeAdapter earthquakeAdapter = new EarthquakeAdapter(this,earthquakes);
         try{
-            earthquakeListView.setAdapter(earthquakeAdapter);
+            if (earthquakeListView != null) {
+                earthquakeListView.setAdapter(earthquakeAdapter);
+            }
+            else{
+                Toast.makeText(EarthquakeActivity.this, "Unable to set onItemClickListener for list items", Toast.LENGTH_LONG).show();
+            }
         }
         catch (NullPointerException e){
             Toast.makeText(this, "Unable to set list adapter", Toast.LENGTH_SHORT).show();
         }
-
     }
 }
